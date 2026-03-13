35166cec8f1327f46c0ea8e9ad255b90761bdd95
Doenwe <joran@doenwe.events>
2026-03-13 19:57:23 +0100

    Update Puzzle.js

--- models/Puzzle.js
+++ models/Puzzle.js
@@ -1,18 +1,20 @@
+// models/Puzzle.js
 import mongoose from "mongoose";
 
 const ModuleSchema = new mongoose.Schema({
   type: { type: String, required: true },
-  data: { type: mongoose.Schema.Types.Mixed, default: {} }   // <-- data i.p.v. content
+  data: { type: mongoose.Schema.Types.Mixed, default: {} }
 }, { _id: false });
 
 const PageSchema = new mongoose.Schema({
   title: { type: String, default: "Pagina" },
+  showNext: { type: Boolean, default: true },         // 👈 NIEUW
   modules: { type: [ModuleSchema], default: [] }
 }, { _id: false });
 
 const PuzzleSchema = new mongoose.Schema({
   name: { type: String, required: true },
   pages: { type: [PageSchema], default: [] }
-}, { timestamps: true });  // createdAt/updatedAt automatisch
+}, { timestamps: true });
 
 export default mongoose.model("Puzzle", PuzzleSchema);
